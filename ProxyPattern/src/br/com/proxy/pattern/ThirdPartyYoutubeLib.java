b5e64fdc9ea61cf978fad419dca2f5ce46b7167a
Kleyton Dias <49427561+kleyton-85@users.noreply.github.com>
2022-03-28 19:56:45 -0400

    Delete ThirdPartyYoutubeLib.java

--- ProxyPattern/src/br/com/proxy/pattern/ThirdPartyYoutubeLib.java
+++
@@ -1,9 +0,0 @@
-package br.com.proxy.pattern;
-
-import java.util.HashMap;
-
-public interface ThirdPartyYoutubeLib {
-	HashMap<String, Video> popularVideos();
-	
-	Video getVideo(String videoId);
-}
