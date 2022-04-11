341c985eee6a69e9b26e267fb97f6d2f80c7d2ff
Kleyton Dias <49427561+kleyton-85@users.noreply.github.com>
2022-04-11 19:47:41 -0400

    Add files via upload

---
+++ ObserverPattern/src/br/com/observer/pattern/EventSource.java
@@ -0,0 +1,29 @@
+package br.com.observer.pattern;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Observable;
+import java.util.Observer;
+
+//@SuppressWarnings({ "deprecation", "unused" })
+public class EventSource extends Observable implements Runnable {
+	
+	
+	public void run() {
+		try {
+			final InputStreamReader isr = new InputStreamReader (System.in);
+			final BufferedReader br = new BufferedReader (isr);
+			while (true) {
+				String response = br.readLine();
+				setChanged();
+				notifyObservers(response);
+			}
+		}
+
+		catch(IOException e) {
+			e.printStackTrace();
+		}
+	}		
+}
+
