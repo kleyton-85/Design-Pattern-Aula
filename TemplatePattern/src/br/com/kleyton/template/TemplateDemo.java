3905663edef68195ff91183a964eff61239e3d7a
Kleyton Dias <49427561+kleyton-85@users.noreply.github.com>
2022-05-16 19:58:08 -0400

    Add files via upload

---
+++ TemplatePattern/src/br/com/kleyton/template/TemplateDemo.java
@@ -0,0 +1,29 @@
+package br.com.kleyton.template;
+
+import java.util.Scanner;
+
+public class TemplateDemo {
+
+	public static void main(String[] args) {
+		
+		String jogo = "";
+		
+		do {
+			System.out.println("\n\n-------------------------------------");
+			System.out.println("  #### Qual jogo Deseja Jogar? ####" + "\n      1 - Xadrez" + "\n      2- Soccer" + "\n\n  --> Escolha seu Jogo: ");
+			Scanner entrada = new Scanner(System.in);
+			jogo = entrada.nextLine();
+			Game game;
+			
+			if (jogo.matches("Xadrez")) {
+				game = new Chess();
+				game.play();
+			
+			} if (jogo.matches("Soccer")){
+				game = new Soccer();
+				game.play();
+			} 
+				
+		} while (!jogo.matches("Sair"));
+	}
+}
