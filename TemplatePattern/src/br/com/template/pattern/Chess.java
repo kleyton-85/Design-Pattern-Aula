4879c1edabb4c9ceda1e199e05fe7d7e721a1b51
Kleyton Dias <49427561+kleyton-85@users.noreply.github.com>
2022-05-16 20:00:49 -0400

    Add files via upload

---
+++ TemplatePattern/src/br/com/template/pattern/Chess.java
@@ -0,0 +1,20 @@
+package br.com.template.pattern;
+
+public class Chess extends Game {
+
+	void initialize() {
+		// TODO Auto-generated method stub
+		System.out.println("\n   --> Jogo de Xadrez inicializado! Start ...");
+	}
+
+	void start() {
+		// TODO Auto-generated method stub
+		System.out.println("   --> Jogo Começou! Bem-vindo ao jogo de Xadrez ...");
+	}	
+
+	void end() {
+		// TODO Auto-generated method stub
+		System.out.println("   --> Xeque-Mate! Jogo Terminou ...");
+	}
+
+}
